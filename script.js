cebc304dd3b59f5ebb87f98473944385e132873f
B4rtekk1 <bartoszkasyna@gmail.com>
2025-06-25 18:41:54 +0200

    update

--- script.js
+++ script.js
@@ -5,3 +5,33 @@ $(document).ready(function() {
         $('.nav-list').slideToggle(300);
     });
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+    const contentDiv = document.getElementById("content");
+
+    fetch("mnist_imports.md")
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error("Failed to load mnist.md");
+            }
+            return response.text();
+        })
+        .then((markdown) => {
+            const htmlContent = marked.parse(markdown, {
+                highlight: (code, lang) => {
+                    const validLanguage = hljs.getLanguage(lang) ? lang : 'plaintext';
+                    return hljs.highlight(code, { language: validLanguage }).value;
+                },
+            });
+
+            contentDiv.innerHTML = htmlContent;
+
+            document.querySelectorAll("pre code").forEach((block) => {
+                hljs.highlightElement(block);
+            });
+        })
+        .catch((error) => {
+            contentDiv.textContent = "Nie udało się załadować pliku mnist.md.";
+            console.error(error);
+        });
+});
