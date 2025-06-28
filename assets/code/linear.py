7f0b4c301c8d9c93f15c8e7d6a14136392d99797
B4rtekk1 <bartoszkasyna@gmail.com>
2025-06-28 19:37:35 +0200

    update

--- assets/code/linear.py
+++ assets/code/linear.py
@@ -8,7 +8,7 @@ np.random.seed(42)
 X = np.random.rand(100, 1) * 10
 true_slope = 2.5
 true_intercept = 5
-y = true_slope * X + true_intercept + np.random.randn(100, 1) * 3
+y = true_slope * X + true_intercept + np.random.randn(100, 1) * 3 # test
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
@@ -16,12 +16,10 @@ model = LinearRegression()
 model.fit(X_train, y_train)
 y_pred = model.predict(X_test)
 
-plt.figure(figsize=(10, 6))
-plt.scatter(X, y, color='blue', label='Actual data')
-plt.plot(X, model.predict(X), color='red', label='Regression line')
-plt.xlabel('X')
-plt.ylabel('y')
-plt.title('Linear Regression on Random Data')
-plt.legend()
-plt.savefig('linear_regression.png', format='png', dpi=300)
+plt.figure(figsize=(16, 9))
+plt.scatter(X, y, color='blue')
+plt.plot(X, model.predict(X), color='red')
+plt.axis('off')
+plt.legend().remove()
+plt.savefig('../images/linear_regression_background.png', format='png', dpi=300, transparent=True)
 plt.show()
